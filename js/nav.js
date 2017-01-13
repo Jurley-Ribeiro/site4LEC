746c8b7b9d984a1236e9d699388afbfafa222f90
Breno Henrique <brenosc2@hotmail.com>
2017-01-13 20:18:37 -0200

    adicionando scrollify nas sections e nos links da navbar

--- js/nav.js
+++ js/nav.js
@@ -6,4 +6,11 @@ $(document).ready(function() {
     $(this).toggleClass('is-active');
     $menu.toggleClass('is-active');
   });
+
+  $(".nav-item").click(function (e) {
+    e.preventDefault();
+    var index = parseInt($(this).attr("href"));
+    $.scrollify.move(index);
+  });
+
 });
---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$(function() {
+  $.scrollify({
+    section: ".hero",
+    sectionName: 'terça',
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset: 0,
+    scrollbars: true,
+    standardScrollElements: "", // Elementos que precisar de Scroll normal.
+  });
+});
