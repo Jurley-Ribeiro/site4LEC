1b5a74b19fd8e6623556a4c7d23e3829094a7251
Breno Henrique <brenosc2@hotmail.com>
2017-01-21 23:21:23 -0200

    font-awesome import without CDN

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('pack-js', function() {
 });
 
 gulp.task('pack-css', function() {
-  return gulp.src(['src/css/reset.css', 'src/css/bulma.css', 'src/css/style.css'])
+  return gulp.src(['src/css/reset.css', 'node_modules/bulma/css/bulma.css', 'node_modules/font-awesome/css/font-awesome.min.css', 'src/css/style.css'])
     .pipe(concat('stylesheet.css'))
     .pipe(gulp.dest('public/css'));
 });
@@ -26,7 +26,7 @@ gulp.task('remove-unused-css', function() {
     .pipe(uncss({
       html: ['src/index.html']
     }))
-    .pipe(gulp.dest('./public/css/stylesheet.min.css'));
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('imagemin', () =>
@@ -48,8 +48,13 @@ gulp.task('sizereport', function() {
     .pipe(sizereport());
 });
 
+gulp.task('import-icon-font', function() {
+  return gulp.src('./node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('public/fonts'))
+});
+
 gulp.task('default', function(done) {
-  runSequence('pack-js', 'pack-css', 'imagemin', 'minify-html', 'remove-unused-css', 'sizereport', function() {
+  runSequence('pack-js', 'imagemin', 'minify-html', 'remove-unused-css', 'import-icon-font', 'pack-css', 'sizereport', function() {
     done();
   });
 });
